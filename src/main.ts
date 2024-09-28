a5f122a9d69bb441d57785aefa6eeef8b475c543
FabrizioAQuispe <fabrizioquispe1900@gmail.com>
2024-09-28 13:18:23 -0500

    ACBT_WEB_INSTITUCIONAL_PROD_v1

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './api/app/app.module';
+import * as dotenv from 'dotenv'
+
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
