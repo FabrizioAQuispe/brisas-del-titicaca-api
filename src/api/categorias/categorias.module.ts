270bf7ac3b21aa6b247a73a6a2f9f72ade215e12
FabrizioAQuispe <alekzander.fabrizio@gmail.com>
2024-10-28 11:39:04 -0500

    Comidas_Apis_v4

--- src/api/categorias/categorias.module.ts
+++ src/api/categorias/categorias.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { CategoriasController } from './categorias.controller';
+import { CategoriasService } from './categorias.service';
 
 @Module({
-    providers: [JwtService],
+    providers: [JwtService,CategoriasService],
     controllers: [CategoriasController],
     exports: [] 
 })
