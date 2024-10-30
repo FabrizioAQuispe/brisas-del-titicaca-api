73bc45a1734d0177d4ff7248889fda6546452ffc
FabrizioAQuispe <alekzander.fabrizio@gmail.com>
2024-10-30 18:26:36 -0500

    EVENTOS_CULTURALES

--- src/api/eventos-culturales/eventos-culturales.controller.ts
+++ src/api/eventos-culturales/eventos-culturales.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
+import { EventosCulturalesService } from './eventos-culturales.service';
+import { EventosCulturales } from '../model/EventosCulturalesDTO';
+import { JwtAuthGuard } from '../utils/Security/JwtService';
 
 @Controller('eventos-culturales')
-export class EventosCulturalesController {}
+export class EventosCulturalesController {
+    constructor(
+        private eventosCulturales: EventosCulturalesService
+    ) { }
+
+    @Get('/list')
+    async getEventosCulturales() {
+        return this.eventosCulturales.getEventosCulturales();
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Post('/create')
+    async createEventosCulturales(eventosCulturales: EventosCulturales) {
+        return this.eventosCulturales.createEventosCulturales(eventosCulturales);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Put('/update/:id')
+    async updateEventosCulturales(@Param('id') id:number,@Body() eventosCulturales:EventosCulturales) {
+        return this.eventosCulturales.updateEventosCulturales(id, eventosCulturales);
+    }
+}
