364a63760596f5385e9880feae6c56c14ee06e17
FabrizioAQuispe <alekzander.fabrizio@gmail.com>
2024-11-04 11:09:56 -0500

    POPUP_JWTAUTHGUARD

--- src/api/popup/popup.controller.ts
+++ src/api/popup/popup.controller.ts
@@ -6,42 +6,45 @@ import { JwtAuthGuard } from '../utils/Security/JwtService';
 @Controller('popup')
 export class PopupController {
     constructor(
-        private popupService:PopupService
-    ){}
+        private popupService: PopupService
+    ) { }
 
-    @UseGuards(JwtAuthGuard)
     @Get('/list')
-    async getPopup(){
+    async getPopup() {
         try {
             return this.popupService.getPopup();
         } catch (error) {
             throw new Error('ERROR CONTROLLER' + error.message)
         }
     }
+    @UseGuards(JwtAuthGuard)
 
     @Post('/create')
-    async createPopup(@Body() popup: Popup){
+    async createPopup(@Body() popup: Popup) {
         try {
             return this.popupService.createPopup(popup);
         } catch (error) {
             throw new Error('ERROR CONTROLLER' + error.message)
         }
     }
+    @UseGuards(JwtAuthGuard)
 
     @Put('/update/:idPopup')
-    async updatePopup(@Param('idPopup') idPopup:number,@Body() popup:Popup){
+    async updatePopup(@Param('idPopup') idPopup: number, @Body() popup: Popup) {
         try {
             return this.popupService.updatePopup(idPopup, popup);
         } catch (error) {
             throw new Error('ERROR CONTROLLER' + error.message)
         }
     }
+    @UseGuards(JwtAuthGuard)
+
     @Delete('/delete/:idPopup')
-    async deletePopup(@Param('idPopup') idPopup:number){
+    async deletePopup(@Param('idPopup') idPopup: number) {
         try {
             return this.popupService.deletePopup(idPopup);
         } catch (error) {
             throw new Error('ERROR CONTROLLER' + error.message)
-        }   
+        }
     }
 }
