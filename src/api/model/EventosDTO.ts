1588344909234991bec6c0f3b6295883fd37303f
FabrizioAQuispe <alekzander.fabrizio@gmail.com>
2024-11-05 15:42:50 -0500

    EVENTOSFECHAS_API_CORREGIDO_V3

--- src/api/model/EventosDTO.ts
+++ src/api/model/EventosDTO.ts
@@ -1,10 +1,11 @@
 export interface Eventos {
+    id?: number;
     nombre: string,
-    slig: string,
-    descripcion: string,
-    image: string,
-    fecha_creacion: Date,
-    fecha_edicion: Date,
-    precio: number,
-    bg_color: string;
+    slig?: string,
+    descripcion?: string,
+    image?: string,
+    fecha_creacion?: Date,
+    fecha_edicion?: Date,
+    precio?: number,
+    bg_color?: string;
 }
