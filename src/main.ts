cc9687a4f65fd1dd67c09cea6efe0c01d0377e98
FabrizioAQuispe <alekzander.fabrizio@gmail.com>
2024-12-17 11:14:43 -0500

    main arreglado

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*',
-    methods: 'GET, PUT, POST , DELETE, PATCH,HEAD',
+    methods: 'GET, PUT, POST , DELETE, PATCH',
     credentials: true
   });
   await app.listen(3000);
